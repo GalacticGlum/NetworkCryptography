f21d39c2d73f04e5501978d7a794d2229fad0dc7
GalacticGlum <nerftactic09@gmail.com>
2017-09-07 11:29:30 -0400

    Project cleanup

--- NetworkCryptography/Client/Properties/AssemblyInfo.cs
+++ NetworkCryptography/Client/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetworkCryptography")]
+[assembly: AssemblyTitle("Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NetworkCryptography")]
+[assembly: AssemblyProduct("Client")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- NetworkCryptography/Client/Properties/Resources.Designer.cs
+++ NetworkCryptography/Client/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NetworkCryptography.Properties
+namespace Client.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace NetworkCryptography.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NetworkCryptography.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Client.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
